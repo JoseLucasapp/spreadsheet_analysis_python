9aac5972b01516c1a055f0f419fd62fe3907fd69
José Lucas <josluc2016@gmail.com>
2024-06-10 01:56:41 -0300

    fix: Utils

---
+++ utils.py
@@ -0,0 +1,7 @@
+class Utils:
+    def autopct_format(self, values):
+        def my_format(pct):
+            total = sum(values)
+            val = int(round(pct*total/100.0))
+            return '{v:d}'.format(v=val)
+        return my_format
