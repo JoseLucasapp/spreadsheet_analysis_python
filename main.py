6ff0182caa4191465c850cfe4ca61901f6fb7bdc
José Lucas <josluc2016@gmail.com>
2024-06-30 03:24:35 -0300

    feat: function to display chart created

--- main.py
+++ main.py
@@ -2,22 +2,28 @@
 
 from tkinter import *
 from tkinter import ttk
+from PIL import Image, ImageTk
 
 from analysis import Analysis
+from convert import Convert
+from chart_generator import Generate_Charts
 
 
 class GUI:
     def __init__(self):
         sheet_link = 'https://docs.google.com/spreadsheets/u/1/d/e/2PACX-1vS5qVKbg9hoLHg00Y5AqZu5XQxylCKHDjlOL0y3MtDRAVHmJcdkCp9tzi5m9kXwES8ObCqplRXHSW4M/pubhtml#'
         self.analysis = Analysis(sheet_link)
+        Convert()
         self.analysis.caller()
+        self.chart_generator = Generate_Charts(self.analysis.sheet_titles)
+        self.chart_generator.caller()
 
         self.root = Tk()
         self.root.minsize(width=500, height=500)
         self.selected = 'Nenhum'
+        self.photo = None
 
-    def select_field(self):
-        options = self.analysis.champions
+    def select_field(self, options):
         self.combobox = ttk.Combobox(
             self.root, values=options)
         self.combobox.pack(pady=20)
@@ -30,10 +36,17 @@ class GUI:
 
     def selected_option(self, event):
         self.selected = self.combobox.get()
+        self.chart_generator.finals_win_percentage(self.selected)
+        image = Image.open('./chart.png')
+        resized = image.resize((400, 400))
+        self.photo = ImageTk.PhotoImage(image=resized)
+
+        label_image_chart = ttk.Label(self.root, image=self.photo)
+        label_image_chart.pack()
         self.label_selected.config(text=f"Selecionado: {self.selected}")
 
     def caller(self):
-        self.select_field()
+        self.select_field(self.analysis.champions)
         self.root.mainloop()
 
 
